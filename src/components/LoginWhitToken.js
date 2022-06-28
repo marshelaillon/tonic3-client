673ea814c361943cb935b9c8f0a19d8de17a88d7
andradeMajo <majjito2003@gmail.com>
2022-06-28 07:28:05 -0500

    loguin Whit token prueba

---
+++ src/components/LoginWhitToken.js
@@ -0,0 +1,174 @@
+import React from 'react';
+import { useRef, useState } from 'react';
+import * as Yup from 'yup';
+import { Formik, Form, Field } from 'formik';
+import { Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from '../state/user/user';
+import ReCAPTCHA from 'react-google-recaptcha';
+
+const LoginWhitToken = () => {
+  //const guestEmails = useSelector(state => state.guestEmails);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [captchaValido, setCaptchaValido] = useState(false);
+  const [usuarioValido, setUsuarioValido] = useState(false);
+  const [validGuest, setValidGuest] = useState(false);
+  const [cantSubmit, setCantSubmit] = useState(false);
+  const captcha = useRef(null);
+
+  const handleSubmit = values => {
+    if(usuarioValido){
+      console.log(values);
+      // guestEmails.forEach(item => {
+    //   if (item.email === values.email) {
+    //     console.log('encontre una coincidencia');
+    //     setValidGuest(true);
+    //   } else {
+    //     console.log('no encontre ninguna coincidencia');
+    //   }
+    // });  
+    } else console.log("NO VALIDO");
+    
+  
+    // if (!captchaValido) {
+    //   setCantSubmit(true);
+    // }
+  };
+
+  const validate = Yup.object({
+    email: Yup.string()
+      .email('El email ingresado no es válido')
+      .required('Se requiere un email'),
+  token:usuarioValido && Yup.string().required("se requiere un token")
+  
+    // password: Yup.string()
+    //   .min(6, 'La contraseña debe tener al menos 6 caracteres')
+    //   .required('Se requiere contraseña'),
+  });
+
+  const onChange = () => {
+    if (('hubo un cambio', captcha.current.getValue())) {
+      console.log('el usuario no es un robot');
+      setCaptchaValido(true);
+    }
+  };
+
+  return (
+    <>
+      <Formik
+        initialValues={{
+          email: '',
+          token: '',
+          //   password: '',
+        }}
+        validationSchema={validate}
+        onSubmit={values => {
+          handleSubmit(values);
+          if (captcha.current.getValue()) {
+            console.log('el usuario no es un robot');
+            setUsuarioValido(true);
+            setCaptchaValido(true);
+          } else {
+            console.log('Aceptar el captcha');
+            setUsuarioValido(false);
+            setCaptchaValido(false);
+          }
+        }}
+      >
+        {formik => (
+          <div className="container w-75 mt-4">
+            <Form>
+              <div className="form-group">
+                <label htmlFor="email">E-mail</label>
+                <Field
+                  name="email"
+                  className={
+                    formik.touched.email && formik.errors.email
+                      ? 'form-control is-invalid'
+                      : 'form-control'
+                  }
+                  type="email"
+                />
+                {formik.touched.email && formik.errors.email ? (
+                  <div className="invalid-feedback">{formik.errors.email}</div>
+                ) : null}
+              </div>
+              {/* {validGuest && (
+                  <div>
+                    <label htmlFor="accesCode">Access Code</label>
+                    <input
+                      type="password"
+                      name="accesCode"
+                      placeholder=""
+                    ></input>
+                  </div>
+                )} */}
+              {/* guestEmails*/}
+              {usuarioValido && (
+                <div className="form-group">
+                  <label htmlFor="loginToken">
+                    Token 
+                    <Field
+                      name="token"
+                      className={
+                        formik.touched.token && formik.errors.token
+                          ? 'form-control is-invalid'
+                          : 'form-control'
+                      }
+                      type="password"
+                    />
+                    {formik.touched.token && formik.errors.token ? (
+                      <div className="invalid-feedback">{formik.errors.token}</div>
+                    ) : null}
+                  </label>
+                </div>
+              )}
+              {/* <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <Field
+                    name="password"
+                    className={
+                      formik.touched.password && formik.errors.password
+                        ? 'form-control is-invalid'
+                        : 'form-control'
+                    }
+                    type="password"
+                  />
+                  {formik.touched.password && formik.errors.password ? (
+                    <div className="invalid-feedback">
+                      {formik.errors.password}
+                    </div>
+                  ) : null}
+                </div> */}
+              <div className="recaptcha">
+                <ReCAPTCHA
+                  ref={captcha}
+                  sitekey="6LdOKZogAAAAAEhkSW2hDBgJlWOncF-Ivg8DSB_r"
+                  onChange={onChange}
+                />
+              </div>
+              {cantSubmit && (
+                <div style={{ color: 'red' }}>Por favor acepta el captcha</div>
+              )}
+              <div className="mt-4 d-flex flex-row">
+                <div className="form-group me-4">
+                  <Button type="submit" variant="dark">
+                    Search
+                  </Button>
+                </div>
+              </div>
+              <p className="mt-4">
+                No tenés una cuenta?&nbsp;
+                <Link to="/register">Te invitamos a registrarte.</Link>
+              </p>
+            </Form>
+          </div>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default LoginWhitToken;
