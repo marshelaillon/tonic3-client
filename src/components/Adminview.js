1dd8bc537888c43bcc8148c3cec81a917fda4b21
AlanGitez <benitez.alanez@gmail.com>
2022-07-01 18:20:49 -0300

    apenas iniciada la adminView

--- src/components/Adminview.js
+++ src/components/Adminview.js
@@ -1,101 +1,49 @@
 import React, { useState } from 'react';
-import { Formik, Form, Field } from 'formik';
-import { Button } from 'react-bootstrap';
-import * as Yup from 'yup';
+import '../styles/adminView.css';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { addEvent } from '../state/admin/eventController/event';
 
 const Adminview = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleSubmit = values => {
-        console.log('LLEGTUEB ESTAS ACA ', values);
-        dispatch(
-            addEvent({
-                title: values.title,
-                url: values.url,
-                description: values.description,
-            })
-        );
-    };
-    const validate = Yup.object({
-        url: Yup.string().url().required('Se nesecita Link del evento '),
-        descrition: Yup.string(),
-        title: Yup.string().required('Titulo/Nombre del evento requerido'),
-    });
-
-    return (
-        <div>
-            <Formik
-                initialValues={{
-                    title: '',
-                    url: '',
-                    description: '',
-                }}
-                validationSchema={validate}
-                onSubmit={values => handleSubmit(values)}
-            >
-                {formik => (
-                    <div className="container w-75 mt-4 form">
-                        <Form>
-                            <div className="form-group">
-                                <label htmlFor="title">Titulo </label>
-                                <Field
-                                    name="title"
-                                    className={
-                                        formik.touched.title && formik.errors.title
-                                            ? 'form-control is-invalid'
-                                            : 'form-control'
-                                    }
-                                    type="text"
-                                />
-                                {formik.touched.title && formik.errors.title ? (
-                                    <div className="invalid-feedback">{formik.errors.title}</div>
-                                ) : null}
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="url">Link del evento </label>
-                                <Field
-                                    name="url"
-                                    className={
-                                        formik.touched.url && formik.errors.url
-                                            ? 'form-control is-invalid'
-                                            : 'form-control'
-                                    }
-                                    type="text"
-                                />
-                                {formik.touched.url && formik.errors.url ? (
-                                    <div className="invalid-feedback">{formik.errors.url}</div>
-                                ) : null}
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="description">Descripción </label>
-                                <Field
-                                    name="description"
-                                    as="textarea"
-                                    className={
-                                        formik.touched.description && formik.errors.description
-                                            ? 'form-control is-invalid'
-                                            : 'form-control'
-                                    }
-                                    type="text"
-                                />
-                                {formik.touched.description && formik.errors.description ? (
-                                    <div className="invalid-feedback">
-                                        {formik.errors.description}
-                                    </div>
-                                ) : null}
-                            </div>
-
-                            <Button type="submit" variant="light">
-                                Crear evento
-                            </Button>
-                        </Form>
-                    </div>
-                )}
-            </Formik>
+  return (
+    <>
+      <header>
+        <div className="container-sm header w-75">
+          <div className="row mt-5">
+            <div className="list ">
+              <ul className="d-flex justify-content-around">
+                <li>Events</li>
+                <li>Guests</li>
+                <li>Users</li>
+              </ul>
+            </div>
+          </div>
         </div>
-    );
+      </header>
+
+      <div className="container content mt-5">
+        <div class="list-group">
+          <a href="#" class="list-group-item active">
+            Cras justo odio
+          </a>
+          <a href="#" class="list-group-item">
+            Dapibus ac facilisis in
+          </a>
+          <a href="#" class="list-group-item">
+            Morbi leo risus
+          </a>
+          <a href="#" class="list-group-item">
+            Porta ac consectetur ac
+          </a>
+          <a href="#" class="list-group-item">
+            Vestibulum at eros
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Adminview;
