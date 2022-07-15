060944541237757d72bb60ea4134f164ef779909
Maxi-Rios <maxirecibos182@gmail.com>
2022-07-15 01:45:59 -0300

    cargas de imagenes

--- src/commons/VideoPlayer.js
+++ src/commons/VideoPlayer.js
@@ -8,31 +8,36 @@ import { Button } from 'react-bootstrap';
 
 const VideoPlayer = () => {
     const currentEvent = useSelector(state => state.currentEvent);
+    const user = useSelector(state => state.user);
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getUserEvents());
     }, []);
     return (
-        <>
-            <div className="player-wrapper">
-                <ReactPlayer
-                    className="videoPlayer"
-                    url={`${currentEvent?.event?.url}`}
-                    playing={true}
-                    controls={true}
-                    width="100%"
-                    height="100%"
-                />
-            </div>
-            <Button variant='transparent'>
-                <a
-                    href={`${currentEvent?.event?.url}`}
-                    target="_blank"
-                    style={{ color: 'orange ', textDecoration: 'none' }}
-                >
-                    Ir la pagina
-                </a>
-            </Button>
+        <>{user.id && (
+            <>
+
+                <div className="player-wrapper">
+                    <ReactPlayer
+                        className="videoPlayer"
+                        url={`${currentEvent?.event?.url}`}
+                        playing={true}
+                        controls={true}
+                        width="100%"
+                        height="100%"
+                    />
+                </div>
+                <Button variant='transparent'>
+                    <a
+                        href={`${currentEvent?.event?.url}`}
+                        target="_blank"
+                        style={{ color: 'orange ', textDecoration: 'none' }}
+                    >
+                        Ir la pagina
+                    </a>
+                </Button>
+            </>
+        )}
         </>
     );
 };
