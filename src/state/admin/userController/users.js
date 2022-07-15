d4326de145eeeeae960715e6db0ae3109f625300
AlanGitez <benitez.alanez@gmail.com>
2022-07-14 22:40:56 -0300

    commit

--- src/state/admin/userController/users.js
+++ src/state/admin/userController/users.js
@@ -6,9 +6,14 @@ export const getUsers = createAsyncThunk(
   'GET_USERS',
   async (undefined, thunkAPI) => {
     const thunk = thunkAPI.getState();
+    const { token } = thunkAPI.getState();
     if (thunk.user.isAdmin) {
       try {
-        const { data } = await axios.get(`${BASE_URL}/admin/get-all-users`);
+        const { data } = await axios.get(`${BASE_URL}/admin/get-all-users`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
         return data?.data;
       } catch (error) {
         console.error('/user/login ERROR ', error);
